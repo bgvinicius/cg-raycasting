cb94c8b34e82496672c932830b9f96a80141e0a4
Amanda <amanda.kevillyn@alu.ufc.br>
2021-03-23 17:34:13 -0300

    Matrix4 creation

--- include/project/matrix4.hpp
+++ include/project/matrix4.hpp
@@ -1,27 +1,142 @@
 #include <vector>
 
+#include "vector3.hpp"
+
 using namespace std;
 
 class Matrix4 {
     public:
-        vector<vector<float>> matrix { vector<float>{0, 0, 0, 0},  vector<float>{0, 0, 0, 0}, vector<float>{0, 0, 0, 0}, vector<float>{0, 0, 0, 0}};
+        vector<vector<float>> index {vector<float>{0, 0, 0, 0},  vector<float>{0, 0, 0, 0}, vector<float>{0, 0, 0, 0}, vector<float>{0, 0, 0, 0}};
+           
 };
 
 Matrix4 identity() {
-    // impl..
+    Matrix4 identity;
+    identity.index[0][0] = 1;
+    identity.index[1][1] = 1;
+    identity.index[2][2] = 1;
+    identity.index[3][3] = 1;
+
+    return identity;
 };
 
 Matrix4 operator+(Matrix4 &u, Matrix4 &v) {
+    Matrix4 resultado;
+    resultado.index[0][0] = u.index[0][0] + v.index[0][0];
+    resultado.index[0][1] = u.index[0][1] + v.index[0][1];
+    resultado.index[0][2] = u.index[0][2] + v.index[0][2];
+    resultado.index[0][3] = u.index[0][3] + v.index[0][3];
+    resultado.index[1][0] = u.index[1][0] + v.index[1][0];
+    resultado.index[1][1] = u.index[1][1] + v.index[1][1];
+    resultado.index[1][2] = u.index[1][2] + v.index[1][2];
+    resultado.index[1][3] = u.index[1][3] + v.index[1][3];
+    resultado.index[2][0] = u.index[2][0] + v.index[2][0];
+    resultado.index[2][1] = u.index[2][1] + v.index[2][1];
+    resultado.index[2][2] = u.index[2][2] + v.index[2][2];
+    resultado.index[2][3] = u.index[2][3] + v.index[2][3];
+    resultado.index[3][0] = u.index[3][0] + v.index[3][0];
+    resultado.index[3][1] = u.index[3][1] + v.index[3][1];
+    resultado.index[3][2] = u.index[3][2] + v.index[3][2];
+    resultado.index[3][3] = u.index[3][3] + v.index[3][3];
+
+    return resultado;   
+}
+
+
+Matrix4 operator-(Matrix4 &u, Matrix4 &v){
+    Matrix4 resultado;
+    resultado.index[0][0] = u.index[0][0] - v.index[0][0];
+    resultado.index[0][1] = u.index[0][1] - v.index[0][1];
+    resultado.index[0][2] = u.index[0][2] - v.index[0][2];
+    resultado.index[0][3] = u.index[0][3] - v.index[0][3];
+    resultado.index[1][0] = u.index[1][0] - v.index[1][0];
+    resultado.index[1][1] = u.index[1][1] - v.index[1][1];
+    resultado.index[1][2] = u.index[1][2] - v.index[1][2];
+    resultado.index[1][3] = u.index[1][3] - v.index[1][3];
+    resultado.index[2][0] = u.index[2][0] - v.index[2][0];
+    resultado.index[2][1] = u.index[2][1] - v.index[2][1];
+    resultado.index[2][2] = u.index[2][2] - v.index[2][2];
+    resultado.index[2][3] = u.index[2][3] - v.index[2][3];
+    resultado.index[3][0] = u.index[3][0] - v.index[3][0];
+    resultado.index[3][1] = u.index[3][1] - v.index[3][1];
+    resultado.index[3][2] = u.index[3][2] - v.index[3][2];
+    resultado.index[3][3] = u.index[3][3] - v.index[3][3];
+
+    return resultado;   
+}
+
+
+Matrix4 operator*(Matrix4 &u, float v) {
+    Matrix4 resultado;
+    resultado.index[0][0] = u.index[0][0] * v;
+    resultado.index[0][1] = u.index[0][1] * v;
+    resultado.index[0][2] = u.index[0][2] * v;
+    resultado.index[0][3] = u.index[0][3] * v;
+    resultado.index[1][0] = u.index[1][0] * v;
+    resultado.index[1][1] = u.index[1][1] * v;
+    resultado.index[1][2] = u.index[1][2] * v;
+    resultado.index[1][3] = u.index[1][3] * v;
+    resultado.index[2][0] = u.index[2][0] * v;
+    resultado.index[2][1] = u.index[2][1] * v;
+    resultado.index[2][2] = u.index[2][2] * v;
+    resultado.index[2][3] = u.index[2][3] * v;
+    resultado.index[3][0] = u.index[3][0] * v;
+    resultado.index[3][1] = u.index[3][1] * v;
+    resultado.index[3][2] = u.index[3][2] * v;
+    resultado.index[3][3] = u.index[3][3] * v;
+
+    return resultado;   
     
 }
 
-// -
-// *
+Matrix4 operator*(float v, Matrix4 &u) {
+    Matrix4 resultado;
+    resultado.index[0][0] = u.index[0][0] * v;
+    resultado.index[0][1] = u.index[0][1] * v;
+    resultado.index[0][2] = u.index[0][2] * v;
+    resultado.index[0][3] = u.index[0][3] * v;
+    resultado.index[1][0] = u.index[1][0] * v;
+    resultado.index[1][1] = u.index[1][1] * v;
+    resultado.index[1][2] = u.index[1][2] * v;
+    resultado.index[1][3] = u.index[1][3] * v;
+    resultado.index[2][0] = u.index[2][0] * v;
+    resultado.index[2][1] = u.index[2][1] * v;
+    resultado.index[2][2] = u.index[2][2] * v;
+    resultado.index[2][3] = u.index[2][3] * v;
+    resultado.index[3][0] = u.index[3][0] * v;
+    resultado.index[3][1] = u.index[3][1] * v;
+    resultado.index[3][2] = u.index[3][2] * v;
+    resultado.index[3][3] = u.index[3][3] * v;
 
-Matrix4 operator*(Matrix4 &m, float v) {
-    // matriz vezes escalar
+    return resultado;   
+    
 }
 
-Matrix4 operator*(Matrix4 &m, Matrix4 &other) {
-    // produtos entree matrizes
+Matrix4 operator*(Matrix4 &u, Matrix4 &v) {
+    Matrix4 resultado;
+    resultado.index[0][0] = u.index[0][0] * v.index[0][0] + u.index[0][1] * v.index[1][0] + u.index[0][2] * v.index [2][0] + u.index[0][3] * v.index[3][0];
+    resultado.index[0][1] = u.index[0][0] * v.index[0][1] + u.index[0][1] * v.index[1][1] + u.index[0][2] * v.index [2][1] + u.index[0][3] * v.index[3][1];
+    resultado.index[0][2] = u.index[0][0] * v.index[0][2] + u.index[0][1] * v.index[1][2] + u.index[0][2] * v.index [2][2] + u.index[0][3] * v.index[3][2];
+    resultado.index[0][3] = u.index[0][0] * v.index[0][3] + u.index[0][1] * v.index[1][3] + u.index[0][2] * v.index [2][3] + u.index[0][3] * v.index[3][3];
+    resultado.index[1][0] = u.index[1][0] * v.index[0][0] + u.index[1][1] * v.index[1][0] + u.index[1][2] * v.index [2][0] + u.index[1][3] * v.index[3][0];
+    resultado.index[1][1] = u.index[1][0] * v.index[0][1] + u.index[1][1] * v.index[1][1] + u.index[1][2] * v.index [2][1] + u.index[1][3] * v.index[3][1];
+    resultado.index[1][2] = u.index[1][0] * v.index[0][2] + u.index[1][1] * v.index[1][2] + u.index[1][2] * v.index [2][2] + u.index[1][3] * v.index[3][2];
+    resultado.index[1][3] = u.index[1][0] * v.index[0][3] + u.index[1][1] * v.index[1][3] + u.index[1][2] * v.index [2][3] + u.index[1][3] * v.index[3][3]; 
+    resultado.index[2][0] = u.index[2][0] * v.index[0][0] + u.index[2][1] * v.index[1][0] + u.index[2][2] * v.index [2][0] + u.index[2][3] * v.index[3][0];
+    resultado.index[2][1] = u.index[2][0] * v.index[0][1] + u.index[2][1] * v.index[1][1] + u.index[2][2] * v.index [2][1] + u.index[2][3] * v.index[3][1];
+    resultado.index[2][2] = u.index[2][0] * v.index[0][2] + u.index[2][1] * v.index[1][2] + u.index[2][2] * v.index [2][2] + u.index[2][3] * v.index[3][2];
+    resultado.index[2][3] = u.index[2][0] * v.index[0][3] + u.index[2][1] * v.index[1][3] + u.index[2][2] * v.index [2][3] + u.index[2][3] * v.index[3][3];
+    resultado.index[3][0] = u.index[3][0] * v.index[0][0] + u.index[3][1] * v.index[1][0] + u.index[3][2] * v.index [2][0] + u.index[3][3] * v.index[3][0];
+    resultado.index[3][1] = u.index[3][0] * v.index[0][1] + u.index[3][1] * v.index[1][1] + u.index[3][2] * v.index [2][1] + u.index[3][3] * v.index[3][1];
+    resultado.index[3][2] = u.index[3][0] * v.index[0][2] + u.index[3][1] * v.index[1][2] + u.index[3][2] * v.index [2][2] + u.index[3][3] * v.index[3][2];
+    resultado.index[3][3] = u.index[3][0] * v.index[0][3] + u.index[3][1] * v.index[1][3] + u.index[3][2] * v.index [2][3] + u.index[3][3] * v.index[3][3];
+
+   return resultado;
+}
+
+Vector3 operator*(Matrix4 &u, Vector3 &v) {
+    return Vector3(u.index[0][0]*v.x + u.index[0][1]*v.y + u.index[0][2]*v.z,
+    u.index[1][0]*v.x + u.index[1][1]*v.y + u.index[1][2]*v.z,
+    u.index[2][0]*v.x + u.index[2][1]*v.y + u.index[2][2]*v.z);
+
 }
